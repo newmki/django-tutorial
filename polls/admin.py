f87899924b24fa1dc8ad39f679c23dc5958f6bda
owen.ki <owen.ki.dev@gmail.com>
2022-12-06 15:15:09 +0900

    Tutorial07: customize the admin change list 1

--- polls/admin.py
+++ polls/admin.py
@@ -9,6 +9,11 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "question_text",
+        "pub_date",
+        "was_published_recently",
+    )
     fieldsets = [
         (
             None,

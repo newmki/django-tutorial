7e19e4abc96499d53012ff5aace4d0f408859f3f
owen.ki <owen.ki.dev@gmail.com>
2022-12-06 15:12:41 +0900

    Tutorial06: adding related objects 2

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (
@@ -22,7 +27,7 @@ class QuestionAdmin(admin.ModelAdmin):
             },
         ),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
